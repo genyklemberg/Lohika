4ff9beb44c7ac5cbc977c4a0e66ea065dc43ad86
Mykola Pelekh <mpelekh@lohika.com>
2017-10-02 18:11:14 +0300

    Issue #8 Create book model

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { IBook } from '../models/book';
 
 @Injectable()
 export class BookService {
-  books: FirebaseListObservable<any[]>;
+  books: FirebaseListObservable<IBook[]>;
 
   constructor(db: AngularFireDatabase) {
     this.books = db.list('/items');
   }
 
-  getBooks(): FirebaseListObservable<any[]> {
+  getBooks(): FirebaseListObservable<IBook[]> {
     return this.books;
   }
 }
